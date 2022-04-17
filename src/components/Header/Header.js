ce276acfe6c8ea5c879656781210ebbe65e7e7e0
mukarrom21 <mukarrom21@gmail.com>
2022-04-17 11:39:43 +0600

    react router installed and some components created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is Header component</h1>
+        </div>
+    );
+};
+
+export default Header;
